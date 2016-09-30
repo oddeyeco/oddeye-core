9f99e10d91a3080c5a85483041361fa0cfaeafa3
vahan <vahan@vahan-system>
2016-09-30 13:44:07 +0400

    Tag class

--- src/main/java/co/oddeye/core/Metric.java
+++ src/main/java/co/oddeye/core/Metric.java
@@ -5,10 +5,38 @@
  */
 package co.oddeye.core;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author vahan
  */
 public class Metric {
+    private String name;
+    private byte[] nameTSDBUID;    
+    private final Map<String,Tag> tags = new HashMap<>();
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the nameTSDBUID
+     */
+    public byte[] getNameTSDBUID() {
+        return nameTSDBUID;
+    }
+
+    /**
+     * @return the tags
+     */
+    public Map<String,Tag> getTags() {
+        return tags;
+    }
     
 }
