33e7cf7d4114fdd8e92918c47ffadcff4edb987d
vahan <vahan@vahan-system>
2016-10-06 12:55:45 +0400

    Calc asinq

--- src/main/java/co/oddeye/core/OddeeyMetricMeta.java
+++ src/main/java/co/oddeye/core/OddeeyMetricMeta.java
@@ -10,6 +10,8 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.stumbleupon.async.Callback;
+import com.stumbleupon.async.Deferred;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,7 +35,15 @@ import net.opentsdb.uid.NoSuchUniqueName;
 import net.opentsdb.uid.UniqueId;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang.ArrayUtils;
+import org.hbase.async.BinaryComparator;
+import org.hbase.async.CompareFilter;
+import org.hbase.async.GetRequest;
+import org.hbase.async.HBaseClient;
 import org.hbase.async.KeyValue;
+import org.hbase.async.QualifierFilter;
+import org.hbase.async.ScanFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -41,6 +51,7 @@ import org.hbase.async.KeyValue;
  */
 public class OddeeyMetricMeta {
 
+    static final Logger LOGGER = LoggerFactory.getLogger(OddeeyMetricMeta.class);
     private String name;
     private byte[] nameTSDBUID;
     private final Map<String, OddeyeTag> tags = new HashMap<>();
@@ -195,8 +206,126 @@ public class OddeeyMetricMeta {
         }
     }
 
+    public void CalculateRulesAsync(long startdate, long enddate, TSDB tsdb) throws Exception {
+        final TSQuery tsquery = new TSQuery();
+
+        tsquery.setStart(Long.toString(startdate));
+        tsquery.setEnd(Long.toString(enddate));
+        final List<TagVFilter> filters = new ArrayList<>();
+        final ArrayList<TSSubQuery> sub_queries = new ArrayList<>();
+        final Map<String, String> querytags = new HashMap<>();
+        final Map<String, SeekableView> datalist = new TreeMap<>();
+        final Calendar CalendarObj = Calendar.getInstance();
+
+        tags.entrySet().stream().forEach((tag) -> {
+            querytags.put(tag.getKey(), tag.getValue().getValue());
+        });
+
+//        querytags.put("UUID", Metric.getAsJsonObject().get("tags").getAsJsonObject().get("UUID").getAsString());
+        TagVFilter.mapToFilters(querytags, filters, true);
+        for (String dsrule : RulesDownsamples) {
+            final TSSubQuery sub_query = new TSSubQuery();
+            sub_query.setMetric(name);
+            sub_query.setAggregator(Aggregator);
+            sub_query.setFilters(filters);
+            sub_query.setDownsample(dsrule);
+//            sub_query.setIndex(0);
+            sub_queries.add(sub_query);
+        }
+
+        tsquery.setQueries(sub_queries);
+        tsquery.validateAndSetQuery();
+        Query[] tsdbqueries = tsquery.buildQueries(tsdb);
+
+        final int nqueries = tsdbqueries.length;
+        final ArrayList<Deferred<DataPoints[]>> deferreds = new ArrayList<>(nqueries);
+        for (int nq = 0; nq < nqueries; nq++) {
+            deferreds.add(tsdbqueries[nq].runAsync());
+        }
+
+        class QueriesCB implements Callback<Object, ArrayList<DataPoints[]>> {
+
+            @Override
+            public Object call(final ArrayList<DataPoints[]> query_results)
+                    throws Exception {
+                double R_value;
+                String family;
+                byte[] time_key;
+                MetriccheckRule RuleItem;
+
+                for (DataPoints[] series : query_results) {
+                    for (final DataPoints datapoints : series) {
+                        try {
+                            Tagmap = datapoints.getTags();
+                            Tagmap.remove("alert_level");
+                            if (!Tagmap.equals(querytags)) {
+                                throw new Exception("Invalid tags");
+                            }
+                        } catch (Exception e) {
+                            throw new Exception(e);
+                        }
+                        final SeekableView Datalist = datapoints.iterator();
+                        while (Datalist.hasNext()) {
+                            final DataPoint Point = Datalist.next();
+                            CalendarObj.setTimeInMillis(Point.timestamp());
+                            family = sub_queries.get(datapoints.getQueryIndex()).downsamplingSpecification().getFunction().toString();
+                            time_key = ByteBuffer.allocate(6).putShort((short) CalendarObj.get(Calendar.YEAR)).putShort((short) CalendarObj.get(Calendar.DAY_OF_YEAR)).putShort((short) CalendarObj.get(Calendar.HOUR_OF_DAY)).array();
+                            R_value = Point.doubleValue();
+                            RuleItem = RulesCache.getIfPresent(Hex.encodeHexString(time_key));
+                            if (RuleItem == null) {
+                                RuleItem = new MetriccheckRule(time_key);
+                            }
+                            RuleItem.update(family, R_value);
+                            RulesCache.put(Hex.encodeHexString(time_key), RuleItem);
+
+                        }
+                    }
+                }
+
+                return null;
+            }
+        }
+        try {
+            Deferred.groupInOrder(deferreds).addCallback(new QueriesCB())
+                    .joinUninterruptibly();
+        } catch (Exception e) {
+            throw new RuntimeException("Shouldn't be here", e);
+        }
+    }
+
     public ConcurrentMap<String, MetriccheckRule> getRulesMap() {
         return RulesCache.asMap();
+    }
+
+    public MetriccheckRule getRule(final Calendar CalendarObj, final byte[] table, final HBaseClient client) throws Exception {
+        final byte[] time_key = ByteBuffer.allocate(6).putShort((short) CalendarObj.get(Calendar.YEAR)).putShort((short) CalendarObj.get(Calendar.DAY_OF_YEAR)).putShort((short) CalendarObj.get(Calendar.HOUR_OF_DAY)).array();
+        MetriccheckRule Rule = RulesCache.getIfPresent(Hex.encodeHexString(time_key));
+        if (Rule == null) {
+            GetRequest get = new GetRequest(table, getKey());
+            ScanFilter filter = new QualifierFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(time_key));
+            get.setFilter(filter);
+            final ArrayList<KeyValue> ruledata = client.get(get).joinUninterruptibly();
+            for (final KeyValue kv : ruledata) {
+                if (kv.qualifier().length != 6) {
+                    continue;
+                }
+
+                Rule = new MetriccheckRule(kv.qualifier());
+
+                // Herdakanucjun@ karevora
+                byte[] b_value = Arrays.copyOfRange(kv.value(), 0, 8);
+                Rule.update("avg", ByteBuffer.wrap(b_value).getDouble());
+                b_value = Arrays.copyOfRange(kv.value(), 8, 16);
+                Rule.update("dev", ByteBuffer.wrap(b_value).getDouble());
+                b_value = Arrays.copyOfRange(kv.value(), 16, 24);
+                Rule.update("min", ByteBuffer.wrap(b_value).getDouble());
+                b_value = Arrays.copyOfRange(kv.value(), 24, 32);
+                Rule.update("max", ByteBuffer.wrap(b_value).getDouble());
+                RulesCache.put(Hex.encodeHexString(kv.qualifier()), Rule);
+                LOGGER.warn("get Rule from Database");
+            }
+        }
+        return Rule;
     }
 
     public void getRulePutValues(byte[][] qualifiers, byte[][] values) {
@@ -250,9 +379,23 @@ public class OddeeyMetricMeta {
         if (this == anObject) {
             return true;
         }
+
         if (anObject instanceof OddeeyMetricMeta) {
             final OddeeyMetricMeta o = (OddeeyMetricMeta) anObject;
-            if (Arrays.equals(o.getNameTSDBUID(), nameTSDBUID) && (tags.equals(o.getTags()))) {
+//            if (Arrays.equals(o.getNameTSDBUID(), nameTSDBUID) && (tags.equals(o.getTags()))) {
+//                return true;
+//            }
+//            if (((tags.get("cluster").equals(o.getTags().get("cluster")))
+//                    && Arrays.equals(o.getNameTSDBUID(), nameTSDBUID)
+//                    && (tags.get("host").equals(o.getTags().get("host")))
+//                    && (tags.get("UUID").equals(o.getTags().get("UUID")))) != (Arrays.equals(o.getNameTSDBUID(), nameTSDBUID) && (tags.equals(o.getTags())))) {
+//                System.out.println(name + "-" + tags);
+//            }
+
+            if ((tags.get("cluster").equals(o.getTags().get("cluster")))
+                    && Arrays.equals(o.getNameTSDBUID(), nameTSDBUID)
+                    && (tags.get("host").equals(o.getTags().get("host")))
+                    && (tags.get("UUID").equals(o.getTags().get("UUID")))) {
                 return true;
             }
         }
